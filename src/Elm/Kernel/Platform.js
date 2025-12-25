63ec817f9abb3e60e3b065d80bd02464e2581fae
Simon Lydell <simon.lydell@gmail.com>
2025-12-25 16:19:32 +0100

    Stop exposing `args` to `_Lamdera_inject`

--- src/Elm/Kernel/Platform.js
+++ src/Elm/Kernel/Platform.js
@@ -188,7 +188,7 @@ function _Platform_initialize(flagDecoder, args, _init, _update, _subscriptions,
 
 	if (typeof _Lamdera_inject === 'function')
 	{
-		_Lamdera_inject(app, callUpdate, model, sendToApp, args);
+		_Lamdera_inject(app, callUpdate, model, sendToApp);
 	}
 
 	return app;
